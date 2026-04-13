d1ab4c1a755752ab6130c9fd3b3cc0b1050fff0f
Gustavo Quirino Dos Santos <gustavo.quirino.santos@escola.pr.gov.br>
2026-04-13 07:56:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,4 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
     
-    // Esconde a tela de login e exibe o conteúdo principal
-    document.getElementById("loginPage").style.display = "none"; 
-    document.getElementById("mainContent").style.display = "block"; 
-
-    // Exibe uma mensagem de login bem-sucedido
-    document.getElementById("loginError").style.display = "block";
-});
-
-// Função para exibir mais informações sobre práticas sustentáveis
-document.getElementById('showMoreBtn').addEventListener('click', function() {
-    const moreInfo = document.getElementById('moreInfo');
-    if (moreInfo.style.display === 'none') {
-        moreInfo.style.display = 'block';
-    } else {
-        moreInfo.style.display = 'none';
-    }
-});
-
-// Função para o contador de sustentabilidade
-let treeCounter = 0;
-let waterCounter = 0;
-
-function updateCounters() {
-    treeCounter += 5; // Adiciona 5 árvores a cada 1 segundo
-    waterCounter += 10; // Adiciona 10 litros de água economizada a cada 1 segundo
-
-    document.getElement
+   
