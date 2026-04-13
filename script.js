de200028a927d17363434a2087d15377e7f0fe54
Gustavo Quirino Dos Santos <gustavo.quirino.santos@escola.pr.gov.br>
2026-04-13 08:01:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// Função de login: Qualquer nome de usuário e senha são aceitos
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-   
+
