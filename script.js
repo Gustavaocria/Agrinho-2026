1347bd3fbc6e4cd9c2460fa411f7ebe860742e00
Gustavo Quirino Dos Santos <gustavo.quirino.santos@escola.pr.gov.br>
2026-04-13 08:04:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function login() {
+  let user = document.getElementById("user").value;
+  let pass = document.getElementById("pass").value;
 
+  if (user === "admin" && pass === "1234") {
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("erro").innerText = "Login inválido!";
+  }
+}
+
+function enviarMensagem(e) {
+  e.preventDefault();
+  document.getElementById("msg").innerText = "Mensagem enviada com sucesso!";
+}
