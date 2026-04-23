87d17a6d2d0e5b90ba39825758fdf2db3d24f933
Gustavo Quirino Dos Santos <gustavo.quirino.santos@escola.pr.gov.br>
2026-04-23 08:21:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// Modo Escuro
-const darkModeToggle = document.getElementById('darkModeToggle');
-darkModeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    darkModeToggle.textContent = document.body.classList.contains('dark-mode') ?
+
